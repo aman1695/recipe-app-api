41fa6325329db61de7147cdee2b1a0c7fec48157
Aman Gupta <39579933+aman1695@users.noreply.github.com>
2020-09-10 19:29:39 +0530

    Last Commit

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
-
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a new user"""
         if not email:
@@ -11,19 +9,14 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
-
         return user
-
     def create_superuser(self, email, password):
         """Create a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-
         return user
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     """Caution user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
